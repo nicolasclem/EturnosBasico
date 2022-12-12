8b42fb52c130eb441745723f600d26fbc13ab786
Nicolas Clementi <clementinicolas@gmail.com>
2022-12-11 23:56:29 -0300

    login hash

--- Eturnos/Controllers/LoginController.cs
+++ Eturnos/Controllers/LoginController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Eturnos.Data;
+using Eturnos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Eturnos.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public LoginController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+          
+        public IActionResult Login(Login login)
+        {
+            if(ModelState.IsValid)
+            {
+                // encriptar pass
+                string passwordEncriptado = Encriptar(login.Password)
+                {
+
+                }
+            }
+            return View("index");
+        }
+
+        private string Encriptar(string pass)
+        {
+            using(SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(pass));
+           
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    stringBuilder.Append(bytes[i].ToString("x2"));
+                }
+                return stringBuilder.ToString();
+            }
         }
     }
 }
